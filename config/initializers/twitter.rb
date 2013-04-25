6e645ffdc1e4aa97dda1ea33b5d63c95fe3f559f
Ryan McAdler <ryanmcadler@gmail.com>
2013-04-24 22:01:45 -0700

    removed more sensitive information and precompile assets from merge

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = "3UfetJb1EzLSiGxdhn3Wg"
-  config.consumer_secret = "x7h7dX5PzD5jKvhDNJ0awGOu2XVRbYeIuj8B8Vk6s"
-  config.oauth_token = "231570812-kfL2c70eHCo2QkXaVAF8RI1c0SPAD018jksSBvye"
-  config.oauth_token_secret = "BBPZs4jI0LCbU6GQgFkRBYrL1mgAwoiTrgmfKTHvxQ"
+  config.consumer_key = ""
+  config.consumer_secret = ""
+  config.oauth_token = ""
+  config.oauth_token_secret = ""
 end
